5c390350aa9574b240b084bad53e33018b6bce06
kchlebicki <krzysztof.chlebicki@gmail.com>
2020-11-16 21:37:45 +0100

    Focus dla formularza kontaktowego

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
-    $(window).scroll(function() {
-        if(this.scrollY >  20) {
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
 
-        if(this.scrollY > 500) {
+        if (this.scrollY > 500) {
             $('.scroll-up').addClass("visible");
         } else {
             $('.scroll-up').removeClass("visible");
         }
     });
 
-    $('.scroll-up').click(function() {
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up').click(function () {
+        $('html').animate({ scrollTop: 0 }, 'fast');
     });
 
-    $('.menu-btn').click(function() {
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    $('.menu a').click(function() {
+    $('.menu a').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-    
+
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
